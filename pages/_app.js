3bc8f9cc3da95e712b490ef5a7804be1b6e26aa0
GeoBrodas <geobro2310@gmail.com>
2021-09-11 16:20:09 +0530

    add og meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,21 @@ function MyApp({ Component, pageProps }) {
     <Provider session={pageProps.session}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Meta tags og */}
+        <meta property="og:title" content="Facebook Clone but better ⚡" />
+        <meta
+          property="og:description"
+          content="This project has a main focus on re-creating the UI with some basic functionality of adding posts, images."
+        />
+        <meta
+          property="og:image"
+          content="https://pbs.twimg.com/media/E-6wWh7VgAU8Dkm?format=jpg&name=4096x4096"
+        />
+        <meta
+          property="og:url"
+          content="https://facebook-clone-geo.vercel.app"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Component {...pageProps} />
     </Provider>
